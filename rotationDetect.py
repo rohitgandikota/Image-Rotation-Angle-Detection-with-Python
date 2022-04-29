8693ccc9dee44f9aa4757e6292e1e3f1819e64e0
Rohit_Gandikota <grohit0@gmail.com>
2022-04-29 17:03:08 +0530

    Updated logic for debugging

--- rotationDetect.py
+++ rotationDetect.py
@@ -14,7 +14,7 @@ import glob
 import os
 
 
-def getAngleofRotation(path, return_images=False, img_list=[]):
+def getAngleofRotation(path, return_images=False, img_list=[], debug=False):
     '''
     Returns the angle the georeferenced image is rotated by
     
@@ -22,10 +22,12 @@ def getAngleofRotation(path, return_images=False, img_list=[]):
             path (str): Path where the image is present
             return_images (bool) [Optional]: The flag to return the rotated image back
             img_list (List) [Optional]: Images list of which the rotation has to be performed on. (Should be absolute path only. Full path)
+            debug (bool) [Optional]: Will return the lines drawn on the image if true
             
         Returns:
             median_angle (float): Float value of the angle by which the image is rotated by
             out_list (List) [Conditional]: List of all rotated images as per input `img_list`. Conditional of `return_images`.
+            debug_img (ndarray) [Conditional]: Image with lines drawn on the jpg. Conditional on `debug` flag.
             
     '''
     # Finding image
@@ -36,11 +38,11 @@ def getAngleofRotation(path, return_images=False, img_list=[]):
     # Reading image and converting to gray scale
     img_before = cv2.imread(img_path)
     img_gray = cv2.cvtColor(img_before, cv2.COLOR_BGR2GRAY)
-    
+    img_gray[img_gray>0] = 255
     # Using edge filtering to bring out sharp features and lines in the image
     img_edges = cv2.Canny(img_gray, 100, 100, apertureSize=3)
     # Detecting lined using Hough Transform
-    lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=100, maxLineGap=5)
+    lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=20, maxLineGap=5)
     
     # Finding angles using slope formula tan(θ) = (y2-y1)/(x2-x1)
     angles = []
@@ -52,6 +54,8 @@ def getAngleofRotation(path, return_images=False, img_list=[]):
     # The algorithm will detect multiple lines and edges. We take the median of all angles
     median_angle = np.median(angles)+90
     
+    if debug:
+        return angle, img_before
     # If return_images is True, rotate the images and return the list
     out_list = []
     if return_images:
@@ -70,11 +74,15 @@ def getAngleofRotation(path, return_images=False, img_list=[]):
             except Exception as e:
                 print(f'Error in rotating images! : {e}')
                 
-        return median_angle, out_list       
+        return median_angle, out_list
     
     return median_angle
 
 if __name__=='__main__':
-    path = 'D:\\Projects\\PQC\\222421531\\222421531\\'
-    angle = getAngleofRotation(path)
+    import matplotlib.pyplot as plt
+    path = 'D:\\Projects\\PQC\\RadQA\\213627031'
+    path = 'D:\\Projects\\DLIP\\RadQA\\data\\211361111'
+    tif_files = glob.glob(os.path.join(path,'*.tif'))
+    angle, outlist = getAngleofRotation(path, return_images=True, img_list=[tif_files[0]])
     print(f"Angle is {angle:.04f}")
+    
